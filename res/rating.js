f3baa0cede71db67594c83879f3f8b1ade8d00f3
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-02-04 17:07:02 +0000

    Cleanup + change to use Prototype $ shortcut
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ratings/trunk@8134 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/rating.js
+++ res/rating.js
@@ -1,19 +1,9 @@
-function submitRating(id, rating, check) {
-	var el = document.getElementById('tx-ratings-display-' + id);
-	el.style.visibility = 'hidden';
-	var el = document.getElementById('tx-ratings-wait-' + id);
-	el.style.visibility = 'visible';
-	//setTimeout('toggleBack(\'' + id + '\')', 2000);
+function tx_ratings_submit(id, rating, check) {
+	$('tx-ratings-display-' + id).style.visibility = 'hidden';
+	$('tx-ratings-wait-' + id).style.visibility = 'visible';
 	new Ajax.Updater('tx-ratings-' + id, 'index.php?eID=tx_ratings_ajax', {
 		asynchronous: true,
 		method: 'post',
 		parameters: 'ref=' + id + '&rating=' + rating + '&data=' + tx_ratings_ajaxData + '&check=' + check
 	});
-}
-
-function toggleBack(id) {
-	var el = document.getElementById('tx-ratings-display-' + id);
-	el.style.visibility = 'visible';
-	var el = document.getElementById('tx-ratings-wait-' + id);
-	el.style.visibility = 'hidden';
 }
