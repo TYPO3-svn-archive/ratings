01277389b74659e3dbdbb536dec704743f67d8fa
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-02-05 09:12:19 +0000

    Release of ratings 1.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ratings/trunk@8153 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "ratings"
 #
-# Auto generated 04-02-2008 15:50
+# Auto generated 05-02-2008 11:10
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Netcreators BV',
-	'version' => '0.3.0',
+	'version' => '1.0.1',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.1.0-100.0.0',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:36:{s:9:"ChangeLog";s:4:"4252";s:25:"class.tx_ratings_ajax.php";s:4:"d016";s:24:"class.tx_ratings_api.php";s:4:"3fb6";s:27:"class.tx_ratings_ttnews.php";s:4:"3ac6";s:12:"ext_icon.gif";s:4:"968d";s:17:"ext_localconf.php";s:4:"3303";s:14:"ext_tables.php";s:4:"7814";s:14:"ext_tables.sql";s:4:"47d7";s:24:"icon_tx_ratings_data.gif";s:4:"968d";s:25:"icon_tx_ratings_iplog.gif";s:4:"75cb";s:13:"locallang.xml";s:4:"eb1f";s:18:"locallang_ajax.xml";s:4:"7ac2";s:16:"locallang_db.xml";s:4:"53c8";s:7:"tca.php";s:4:"3c40";s:14:"doc/manual.sxw";s:4:"d41d";s:38:"modfunc1/class.tx_ratings_modfunc1.php";s:4:"6b74";s:22:"modfunc1/locallang.xml";s:4:"aaa4";s:14:"pi1/ce_wiz.gif";s:4:"2328";s:28:"pi1/class.tx_ratings_pi1.php";s:4:"238e";s:36:"pi1/class.tx_ratings_pi1_wizicon.php";s:4:"f9a0";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"c436";s:19:"res/komodomedia.css";s:4:"1a66";s:19:"res/komodomedia.gif";s:4:"5e11";s:20:"res/masugadesign.css";s:4:"eb59";s:20:"res/masugadesign.gif";s:4:"eed2";s:13:"res/rating.js";s:4:"3ee1";s:20:"res/rating_stars.gif";s:4:"8593";s:16:"res/ratings.html";s:4:"5621";s:21:"res/ratings_test.html";s:4:"02ff";s:14:"res/styles.css";s:4:"a37c";s:15:"res/working.gif";s:4:"70d9";s:17:"res/yvoschaap.css";s:4:"96f1";s:17:"res/yvoschaap.gif";s:4:"0611";s:28:"static/Ratings/constants.txt";s:4:"e1be";s:24:"static/Ratings/setup.txt";s:4:"500a";}',
+	'_md5_values_when_last_written' => 'a:39:{s:9:"ChangeLog";s:4:"e139";s:25:"class.tx_ratings_ajax.php";s:4:"30f0";s:24:"class.tx_ratings_api.php";s:4:"dd11";s:27:"class.tx_ratings_ttnews.php";s:4:"3ac6";s:21:"ext_conf_template.txt";s:4:"5c95";s:12:"ext_icon.gif";s:4:"968d";s:17:"ext_localconf.php";s:4:"3303";s:14:"ext_tables.php";s:4:"fa37";s:14:"ext_tables.sql";s:4:"47d7";s:24:"icon_tx_ratings_data.gif";s:4:"968d";s:25:"icon_tx_ratings_iplog.gif";s:4:"75cb";s:13:"locallang.xml";s:4:"eb1f";s:18:"locallang_ajax.xml";s:4:"7ac2";s:16:"locallang_db.xml";s:4:"f6b4";s:7:"tca.php";s:4:"d0de";s:14:"doc/manual.sxw";s:4:"d41d";s:13:"doc/stars.png";s:4:"04e2";s:38:"modfunc1/class.tx_ratings_modfunc1.php";s:4:"6b74";s:22:"modfunc1/locallang.xml";s:4:"aaa4";s:14:"pi1/ce_wiz.gif";s:4:"2328";s:28:"pi1/class.tx_ratings_pi1.php";s:4:"5986";s:36:"pi1/class.tx_ratings_pi1_wizicon.php";s:4:"f9a0";s:13:"pi1/clear.gif";s:4:"cc11";s:19:"pi1/flexform_ds.xml";s:4:"4b69";s:17:"pi1/locallang.xml";s:4:"70fb";s:20:"res/komodeomedia.css";s:4:"389c";s:20:"res/komodeomedia.gif";s:4:"5e11";s:20:"res/masugadesign.css";s:4:"a394";s:20:"res/masugadesign.gif";s:4:"eed2";s:13:"res/rating.js";s:4:"435d";s:20:"res/rating_stars.gif";s:4:"8593";s:16:"res/ratings.html";s:4:"f2e5";s:21:"res/ratings_test.html";s:4:"02ff";s:14:"res/styles.css";s:4:"666d";s:15:"res/working.gif";s:4:"70d9";s:17:"res/yvoschaap.css";s:4:"4b99";s:17:"res/yvoschaap.gif";s:4:"0611";s:28:"static/Ratings/constants.txt";s:4:"81ae";s:24:"static/Ratings/setup.txt";s:4:"4453";}',
 	'suggests' => array(
 	),
 );
